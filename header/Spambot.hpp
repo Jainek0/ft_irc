557ebc4179dfb1abce64cf94091b41f7aa352a90
Leo Dupont <ledupont@2G5.42angouleme.fr>
2026-06-05 19:01:50 +0200

    blueprint

---
+++ header/Spambot.hpp
@@ -0,0 +1,50 @@
+#ifndef SPAMBOT_HPP
+# define SPAMBOT_HPP
+
+# include "irc.hpp"
+
+class Spambot
+{
+	public:
+		//constructor/destructor
+		Spambot();
+		Spambot(Spambot &other);
+		Spambot& operator=(Spambot &other);
+		~Spambot();
+
+		void joinChannel(std::string channel);
+		{
+			putmsg(fdServ, "JOIN #channel");
+			putmsg(fdServ, "PRIVATEMSG #channel :coucou les amis, il faut que je vous parle de ma nouvelle crypto");
+		}
+		void spam();
+		{
+
+		}
+		typedef std::map<std::string, std::string> box;
+		void overreact(std::string msg);
+		{
+			std::map<std::string, std::string> shout;
+			shout.insert("pile", "macron explosion face");
+			shout.insert("face", "macron decapitation pile");
+			std::ostringstream os(msg);
+			for (std::string str << os; str; str << os)
+			{
+				box::iterator it shout.find(str);
+				if (it != shout.end()) {
+					putmsg(it->second);
+
+				}
+			}
+		}
+		
+		//getters/setters
+		const std::string	&getNickname(void)const {return(_nick);}
+		void				setNickname(std::string nick) {_nick = nick;}
+		
+	private:
+		int 			_fd;
+		std::string 	_nick;
+};
+
+#endif
